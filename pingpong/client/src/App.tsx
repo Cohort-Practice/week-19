49d337e59af772315e0e2e42e0d4de040fc0b7cb
arijiiiitttt🍉 <arijit123roy098@gmail.com>
2026-01-10 18:03:37 +0530

    completed ping pong

--- pingpong/client/src/App.tsx
+++ pingpong/client/src/App.tsx
@@ -1,33 +1,47 @@
-import { useEffect, useState } from "react";
-import "./App.css";
+import React, { useEffect, useState, useRef } from "react";
 
 function App() {
+  const [socket, setSocket] = useState<WebSocket | null>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
 
-  const [ socket , setSocket] = useState();
   function sendMessage() {
-    if(!socket) {
-      return ;
+    if (!socket || socket.readyState !== WebSocket.OPEN || !inputRef.current) {
+      return;
     }
-   
-    socket.send("ping")
+    const message = inputRef.current.value;
+    socket.send(message);
   }
 
   useEffect(() => {
     const ws = new WebSocket("ws://localhost:8080");
+    setSocket(ws);
+
     ws.onmessage = (ev) => {
       alert(ev.data);
+    };
+
+    return () => {
+      ws.close();
     };
   }, []);
 
   return (
-    <>
-      <div className='min-h-screen flex flex-col justify-center items-center'>
-        <div className="flex space-x-2 flex-row">
-          <input className="w-70 h-8 rounded-md bg-gray-100 border border-gray-400" type="text" placeholder="Message ..." />
-        <button className="w-20 h-8 rounded-md bg-blue-500 text-white " onClick={sendMessage}>send</button>
-        </div>
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      <div className="flex space-x-2 flex-row">
+        <input
+          ref={inputRef}
+          className="w-72 h-8 rounded-md bg-gray-100 border border-gray-400"
+          type="text"
+          placeholder="Message ..."
+        />
+        <button
+          className="w-20 h-8 rounded-md bg-blue-500 text-white cursor-pointer"
+          onClick={sendMessage}
+        >
+          send
+        </button>
       </div>
-    </>
+    </div>
   );
 }
 
